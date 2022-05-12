9596c3affc24686f45ecb010d9e1e6b42ba669b1
GEDISER Douala <gediserdouala2021@gmail.com>
2022-05-12 16:16:31 +0200

    force to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (App::environment('production')) {
+            // The environment is production
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
