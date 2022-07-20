de8e2737f21e72889e3d93b79bcd08a3b4e74055
GEDISER Douala <gediserdouala2021@gmail.com>
2022-07-20 18:06:15 +0200

    debut administration admin publications

---
+++ database/migrations/2022_07_20_093932_add_activate_column_to_publications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddActivateColumnToPublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            //
+            $table->boolean('activate')->default(false)->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            //
+            $table->dropColumn('activate');
+        });
+    }
+}
